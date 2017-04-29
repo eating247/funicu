9237f4e41f84e7c2482f2ba387bbf64fc7f71b46
Leo Ahnn <leo.ahnn@gmail.com>
2017-04-29 17:23:37 -0400

    twilio texts working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,25 +2,26 @@ require 'twilio-ruby'
 
 class MessagesController < ApplicationController
   # include Webhookable
-
+  skip_before_action :verify_authenticity_token
   def index
     messages = Message.all
   end
 
   def create
     context = params['Body']
-    message = Message.new(context: context)
-    if message.save
-      puts 'yay'
-    else
-      puts 'boo'
+    message = Message.new(context: context.to_s)
+    puts context
+    message.save
+    response = Twilio::TwiML::Response.new do |r|
+      r.Message "this is what you sound like #{context}"
     end
-    redirect_to :index
+    render :xml => response.to_xml
+    # redirect_to :index
   end
 
   private
-
-  def message_params
-    params.require(:message).permit(:body)
-  end
+  #
+  # def message_params
+  #   params.require(:message).permit('Body')
+  # end
 end
