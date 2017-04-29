8759e20a778af629b8125779b477cf2c9f13384c
Leo Ahnn <leo.ahnn@gmail.com>
2017-04-29 18:44:52 -0400

    fixed associations

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,15 +3,16 @@ require 'twilio-ruby'
 class MessagesController < ApplicationController
   # include Webhookable
   skip_before_action :verify_authenticity_token
-  
+
   def index
     @messages = Message.all
   end
 
   def create
+    puts params["From"][2..-1]
     user = User.find_by(phone_number: params["From"][2..-1])
     context = params['Body']
-    message = Messages.new(context: context.to_s, user_id: user.id)
+    message = user.messages.new(context: context.to_s)
     puts context
     message.save
     response = Twilio::TwiML::Response.new do |r|
