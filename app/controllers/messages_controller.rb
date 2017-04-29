5f4482b51d66fbf8d6e450ea4908279f199a4286
tt <ttwang.303@gmail.com>
2017-04-29 18:32:20 -0400

    add devise users

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,25 +3,21 @@ require 'twilio-ruby'
 class MessagesController < ApplicationController
   # include Webhookable
   skip_before_action :verify_authenticity_token
+  
   def index
-    messages = Message.all
+    @messages = Message.all
   end
 
   def create
+    user = User.find_by(phone_number: params["From"][2..-1])
     context = params['Body']
-    message = Message.new(context: context.to_s)
+    message = Messages.new(context: context.to_s, user_id: user.id)
     puts context
     message.save
     response = Twilio::TwiML::Response.new do |r|
       r.Message "this is what you sound like #{context}"
     end
     render :xml => response.to_xml
-    # redirect_to :index
   end
 
-  private
-  #
-  # def message_params
-  #   params.require(:message).permit('Body')
-  # end
 end
