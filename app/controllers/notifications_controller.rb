9dbd35db4d48a8c1c46607eec58542b6f19470a1
Leo Ahnn <leo.ahnn@gmail.com>
2017-05-27 15:22:37 -0400

    getting app to send msgs

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,23 @@
+require 'twilio-ruby'
+
+class NotificationsController < ApplicationController
+
+  def create
+    to = params['to']
+    message = params['message']
+    client = Twilio::REST::Client.new(
+      ENV['TWILIO_SID'],
+      ENV['TWILIO_TOKEN']
+    )
+    client.messages.create(
+      to: to,
+      from: '+14245431485',
+      body: message
+    )
+  end
+
+  private
+  def notifications_params
+    params.require(:notification).permit(:to, :message)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  
+
   root 'messages#index'
   resources :messages
+  resources :notifications
 end
